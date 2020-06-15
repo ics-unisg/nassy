f5090b2f535fd94f3286ae9ae7155e29415d7499
Uli <c62459@uibk.ac.at>
2020-06-15 18:50:55 +0200

    python setting prop added

---
+++ TestForCheetah/help/setting.py
@@ -0,0 +1,11 @@
+httpPrefix="localhost:8989"
+
+hostName = "localhost"
+
+def getPathToFiles(string):
+    if(hostName=="localhost"):
+        return ".."+string
+
+schemaName= "dcap2"
+dbUser="dcapUser"
+dbPassword="dcap"
